c44767c02f643a87cc49ced8e2964684b326a409
AbhijitRadke <107233937+AbhijitRadke@users.noreply.github.com>
2022-11-28 14:35:41 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const route = require('./routes/route.js');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://user-open-to-all-trainees:AutogenerateSecurePassword@training-cluster.xohin.mongodb.net/sabihaDatabase?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => console.log('mongodb is connected'))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
 
